7ab4fb1702d3d55858b1a3207bb6dd4e49aec753
mehtatanay15 <mehtatanay04@gmail.com>
2025-07-28 20:12:57 +0530

    congrats page finialised

--- siddharth-paul/src/components/afterpayment/pm/Mainpm.jsx
+++ siddharth-paul/src/components/afterpayment/pm/Mainpm.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import Header from '../../Header';
+import Footer from '../../Footer';
 import Congrats from './congrats';
 import PersonalCallsSection from './PersonalCallSection';
 import Discount from './Discount';
 import PersonalCallsSection2 from './PersonalCallsSection2';
 import ThankYou from './ThankYou';
+import '../AfterPayment.css';
 
 const Mainpm = () => {
   return (
-    <div>
+    <div className="afterpayment-page">
+        <Header />
         <Congrats />
         <PersonalCallsSection />
         <Discount />
         <Congrats />
         <PersonalCallsSection2/>
         <ThankYou />
+        <Footer />
     </div>
   )
 }
