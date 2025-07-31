89f9a6a007d67ad8db5825b2d420271f04352b74
Yash Manek <yashmanek2001@gmail.com>
2025-07-31 19:21:47 +0530

    idk

--- controllers/clientInfoController.js
+++ controllers/clientInfoController.js
@@ -2,11 +2,25 @@ import { addOrder } from "../googleSheets/orderSheetUtils.js";
 
 export const submitClientInfo = async (req, res) => {
   try {
-    const { fullName, email, contactInfo, address } = req.body;
+    const {
+      fullName,
+      email,
+      contactInfo,
+      address,
+      source = "unknown",
+    } = req.body;
+
     if (!fullName || !email || !contactInfo || !address) {
       return res.status(400).json({ error: "All fields are required" });
     }
-    await addOrder("ClientInfo", { fullName, email, contactInfo, address });
+
+    await addOrder("ClientInfo", {
+      fullName,
+      email,
+      contactInfo,
+      address,
+      source,
+    });
     res.json({ success: true });
   } catch (error) {
     console.error("Error submitting client info:", error);
